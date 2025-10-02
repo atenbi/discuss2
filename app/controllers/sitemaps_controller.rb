ea10130c45742bbc5deea69d7b7fbbd10639dec5
bbronek <barbro214@gmail.com>
2025-10-02 13:58:05 +0200

    Update sitemap redirect url to use public_url

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,6 +1,6 @@
 class SitemapsController < ApplicationController
   def redirect_to_r2
-    r2_url = "#{Rails.application.credentials.cloudflare[:r2_url]}/sitemaps/sitemap.xml.gz"
+    r2_url = "#{Rails.application.credentials.cloudflare[:public_url]}/sitemaps/sitemap.xml.gz"
     redirect_to r2_url, status: :moved_permanently, allow_other_host: true
   end
 end
